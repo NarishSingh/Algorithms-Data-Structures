62e140372aeb32ef565f0592aa08682ca89d7b2c
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-12-03 19:55:52 -0500

    plus minus hacker rank solution

---
+++ AlgoDS/src/main/java/main/hackerrank/plusminosratios.java
@@ -0,0 +1,38 @@
+package main.hackerrank;
+
+public class plusminosratios {
+
+    static void plusMinus(int[] arr) {
+        int n = arr.length;
+        int posCt = 0;
+        int negCt = 0;
+        int zCt = 0;
+
+        for (int i : arr) {
+            if (i != 0) {
+                switch (Integer.signum(i)){
+                    case -1:
+                        negCt++;
+                        break;
+                    case 1:
+                        posCt++;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Not a number");
+                }
+            } else {
+                zCt++;
+            }
+        }
+
+        System.out.printf("%.6f%n", (double) posCt / n);
+        System.out.printf("%.6f%n", (double) negCt / n);
+        System.out.printf("%.6f%n", (double) zCt / n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-4, 3, -9, 0, 4, 1};
+
+        plusMinus(arr);
+    }
+}
