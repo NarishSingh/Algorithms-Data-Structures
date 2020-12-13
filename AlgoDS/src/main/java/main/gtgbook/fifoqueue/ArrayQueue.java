8e88d79f9fcdad6ece9b3af84abbfa89e82ab38c
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-12-13 18:49:20 -0500

    cloned() and equals() implemented for ArrayQueue

--- AlgoDS/src/main/java/main/gtgbook/fifoqueue/ArrayQueue.java
+++ AlgoDS/src/main/java/main/gtgbook/fifoqueue/ArrayQueue.java
@@ -3,7 +3,10 @@ To solve the problem of removing elements from the front, we use the array circu
  */
 package main.gtgbook.fifoqueue;
 
-public class ArrayQueue<E> implements Queue<E> {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayQueue<E> implements Queue<E>, Cloneable {
 
     private E[] data;
     private int f = 0; //ref to front
@@ -61,5 +64,55 @@ public class ArrayQueue<E> implements Queue<E> {
         f = (f + 1) % data.length; //computes next opening
         sz--;
         return answer;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayQueue{" +
+                "data=" + Arrays.toString(data) +
+                ", f=" + f +
+                ", sz=" + sz +
+                '}';
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected ArrayQueue<E> clone() throws CloneNotSupportedException {
+        //create shallow copy
+        ArrayQueue<E> other = (ArrayQueue<E>) super.clone();
+
+        if (this.size() > 0) {
+            System.arraycopy(this.data, 0, other.data, 0, this.size());
+        }
+
+        return other;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //null test
+        if (o == null) {
+            return false;
+        }
+
+        //type test, if pass -> cast
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        ArrayQueue<?> other = (ArrayQueue<?>) o;
+
+        //size test
+        if (this.size() != other.size()) {
+            return false;
+        }
+
+        //walk and element test
+        for (int i = 0; i < this.size(); i++) {
+            if (this.data[i] != other.data[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- AlgoDS/src/main/java/main/gtgbook/fifoqueue/aqmain.java
+++ AlgoDS/src/main/java/main/gtgbook/fifoqueue/aqmain.java
@@ -4,7 +4,6 @@ public class aqmain {
 
     public static void main(String[] args) {
         Queue<Integer> q = new ArrayQueue<>(3);
-
         q.enqueue(1);
         q.enqueue(2);
         q.enqueue(3);
@@ -16,6 +15,19 @@ public class aqmain {
         q.dequeue();
         System.out.println(q.first());
 
+        //clone and equals
+        ArrayQueue<Integer> orig = new ArrayQueue<>(3);
+        orig.enqueue(4);
+        orig.enqueue(5);
+        orig.enqueue(6);
+        System.out.println("\nOriginal: " + orig.toString());
+        try {
+            ArrayQueue<Integer> cloned = orig.clone();
+            System.out.println("Cloned: " + cloned.toString());
+            System.out.println("Equals?: " + orig.equals(cloned));
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
